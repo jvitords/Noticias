427f076176f3807fcdb63eed7dafd4367d5aad36
João Vitor Santos <jvduarte.santos@hotmail.com>
2024-04-10 13:24:25 -0300

    Adição dos efeitos de hover, estilização da terceira seção e término do projeto

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const botaoFechar = document.querySelector("header nav button");
 const body = document.querySelector("body");
 const nav = document.querySelector("header .mobile ");
 
-console.log(body);
-
 botaoAbrir.addEventListener("click", abrirMenu);
 botaoFechar.addEventListener("click", fecharMenu);
 
